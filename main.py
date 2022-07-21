abe54fb708b3964bf2c72f553e560baddfd8b99f
Victor Abdukarimov <viredmi@gmail.com>
2022-07-21 20:22:01 +0300

    Composed func to one for  post to vk

--- main.py
+++ main.py
@@ -34,34 +34,72 @@ def get_from_vk(method: str, payload: dict) -> dict:
     response = requests.get(url, params=payload)
     response.raise_for_status()
     response_params = response.json()
+    if check_vk_errors(response_params):
+        return check_vk_errors(response_params)
     return response_params["response"]
 
 
-def post_image_to_vkserver(url: str, filename: str) -> dict:
+def post_image_to_vk(filename: str) -> dict:
+    upload_server_params = get_from_vk(
+            method="photos.getWallUploadServer",
+            payload={
+                "access_token": VK_TOKEN,
+                "group_id": VK_GROUP_ID,
+                "v": VK_API_VERSION
+            }
+        )
     with open(filename, 'rb') as file:
         files = {'photo': file}
-        response = requests.post(url, files=files)
-    response.raise_for_status()
-    return response.json()
-
-
-def post_to_vk(method: str, payload: dict) -> dict:
-    response = requests.post(
-        url = f"https://api.vk.com/method/{method}",
-        params=payload,
+        response_server_params = requests.post(
+            upload_server_params["upload_url"],
+            files=files
+        )
+    response_server_params.raise_for_status()
+    server_params = response_server_params.json()
+    if check_vk_errors(server_params):
+        return check_vk_errors(server_params)
+    save_image_response = requests.post(
+        url = "https://api.vk.com/method/photos.saveWallPhoto",
+        params={
+            "access_token": VK_TOKEN,
+            "v": VK_API_VERSION,
+            "group_id": VK_GROUP_ID,
+            "server": server_params["server"],
+            "photo": server_params["photo"],
+            "hash": server_params["hash"]
+        }
     )
-    response.raise_for_status()
-    response_params = response.json()
-    return response_params
+    save_image_response.raise_for_status()
+    if check_vk_errors(save_image_response.json()):
+        return check_vk_errors(save_image_response.json())
+    saved_image_params = save_image_response.json()["response"][0]
+    image_owner_id = saved_image_params["owner_id"]
+    image_id = saved_image_params["id"]
+    post_image_response = requests.post(
+        url = "https://api.vk.com/method/wall.post",
+        params={
+            "access_token": VK_TOKEN,
+            "v": VK_API_VERSION,
+            "owner_id": -(VK_GROUP_ID),
+            "from_group": 1,
+            "attachments": [
+                f"photo{image_owner_id}_{image_id}",
+            ],
+            "message": f"{comics_information['comment']}"
+        }
+    )
+    post_image_response.raise_for_status()
+    if check_vk_errors(post_image_response.json()):
+        return check_vk_errors(post_image_response.json())
+    return post_image_response.json()
 
 
-def check_vk_errors(vk_response_params: dict) -> dict:
+def check_vk_errors(vk_response_params: dict) -> dict | None:
     if vk_response_params.get("error"):
         raise VKWrongDataException(
             f"Код ошибки: {vk_response_params['error']['error_code']}",
             f"Текст ошибки: {vk_response_params['error']['error_msg']}"
         )
-    return vk_response_params
 
 
 if __name__ == "__main__":
@@ -71,43 +109,6 @@ if __name__ == "__main__":
         VK_GROUP_ID = int(os.getenv("VK_GROUP_ID"))
         random_comics_id = random.choice(range(FIRST_COMICS, COUNTED_COMICS))
         comics_information = fetch_xkcd(random_comics_id)
-        vk_upload_server_params = check_vk_errors(get_from_vk(
-            method="photos.getWallUploadServer",
-            payload={
-                "access_token": VK_TOKEN,
-                "group_id": VK_GROUP_ID,
-                "v": VK_API_VERSION
-            }
-        ))
-        vk_server_params = check_vk_errors(post_image_to_vkserver(
-            vk_upload_server_params["upload_url"],
-            filename=comics_information["image_file"]
-        ))
-        vk_saved_image_params = post_to_vk(
-            method="photos.saveWallPhoto",
-            payload={
-                "access_token": VK_TOKEN,
-                "v": VK_API_VERSION,
-                "group_id": VK_GROUP_ID,
-                "server": vk_server_params["server"],
-                "photo": vk_server_params["photo"],
-                "hash": vk_server_params["hash"]
-            }
-        )["response"][0]
-        image_owner_id = vk_saved_image_params['owner_id']
-        image_id = vk_saved_image_params['id']
-        check_vk_errors(post_to_vk(
-            method="wall.post",
-            payload={
-                "access_token": VK_TOKEN,
-                "v": VK_API_VERSION,
-                "owner_id": -(VK_GROUP_ID),
-                "from_group": 1,
-                "attachments": [
-                    f"photo{image_owner_id}_{image_id}",
-                ],
-                "message": f"{comics_information['comment']}"
-            }
-        ))
+        post_image_to_vk(comics_information["image_file"])
     finally:
         removed_image_file = os.remove(comics_information["image_file"])
